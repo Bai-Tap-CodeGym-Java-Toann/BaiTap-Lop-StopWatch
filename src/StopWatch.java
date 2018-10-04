ec2f528e3d766a32b8254156d0531800ebefbcff
Nguyễn Đại Toàn <sitdownrightnow2552@gmail.com>
2018-10-04 18:44:26 +0700

    done

---
+++ src/StopWatch.java
@@ -0,0 +1,27 @@
+public class StopWatch {
+    private static double Startime;
+    private static double Endtime;
+
+    public StopWatch() {
+    }
+
+    public double getStartime() {
+        return Startime;
+    }
+
+    public double getEndtime() {
+        return Endtime;
+    }
+
+    public static void start() {
+        Startime = System.currentTimeMillis();
+    }
+
+    public static void end() {
+        Endtime = System.currentTimeMillis();
+    }
+
+    public static double getElapsedTime() {
+        return Endtime - Startime;
+    }
+}
